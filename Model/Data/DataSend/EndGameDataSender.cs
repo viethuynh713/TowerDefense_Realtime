d7607f22bffedd178caf1959fad0e01d3ad53bec
viethuynh713 <66113968+viethuynh713@users.noreply.github.com>
2023-07-30 00:24:33 +0700

    Update EndGameDataSender.cs

--- Model/Data/DataSend/EndGameDataSender.cs
+++ Model/Data/DataSend/EndGameDataSender.cs
@@ -1,8 +1,12 @@
-﻿namespace Game_Realtime.Model.Data.DataSend;
+﻿using Game_Realtime.Model.InGame;
+
+namespace Game_Realtime.Model.Data.DataSend;
 
 public class EndGameDataSender
 {
     public string gameId;
+    public ModeGame mode;
+
     public string playerWin;
     public int totalTime;
     
